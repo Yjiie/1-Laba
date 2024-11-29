e869af65c9e1bc7bf55f5e4248ab7c50b9080d86
Yjiie <andre.kiseleov@yandex.ru>
2024-11-29 16:43:01 +0400

    Update 1 Laba.py

--- 1 Laba.py
+++ 1 Laba.py
@@ -7,6 +7,7 @@
 Четные двоичные числа, не превышающие 204810, у которых вторая справа цифра равна 0. Выводит на экран цифры числа, исключая нули. Вычисляется среднее число между минимальным и максимальным и выводится прописью.
 '''
 def digit_to_text(digit):
+    """Функция преобразует цифры в пропись."""
     digit_dict = {
         '0': 'ноль', '1': 'один', '2': 'два', '3': 'три', '4': 'четыре',
         '5': 'пять', '6': 'шесть', '7': 'семь', '8': 'восемь', '9': 'девять'
@@ -17,24 +18,34 @@ buffer_len = 1
 work_buffer = ""
 valid_numbers = []
 
-with open("laba.txt", "r") as file:
+with open("text.txt", "r") as file:
     buffer = file.read(buffer_len)
     while buffer:
-        while buffer >= '0' and buffer <= '9':
+        while buffer.isdigit() or (buffer == '-' and not work_buffer):
             work_buffer += buffer
             buffer = file.read(buffer_len)
 
         if len(work_buffer) > 0:
-            if all(c in "01" for c in work_buffer):
-                decimal_value = int(work_buffer, 2)
+            if work_buffer[0] == '-':
+                binary_part = work_buffer[1:]
+            else:
+                binary_part = work_buffer
+
+            if binary_part and all(c in "01" for c in binary_part):
+                decimal_value = int(binary_part, 2)
+                if work_buffer[0] == '-':
+                    decimal_value = -decimal_value
+
                 if (
-                    decimal_value <= 2048 and
+                    abs(decimal_value) <= 2048 and
                     decimal_value % 2 == 0 and
-                    len(work_buffer) > 1 and work_buffer[-2] == '0'
+                    len(binary_part) > 1 and binary_part[-2] == '0'
                 ):
                     if decimal_value not in valid_numbers:
                         valid_numbers.append(decimal_value)
-                        print("Цифры числа, исключая нули:", " ".join(c for c in work_buffer if c != '0'))
+                        filtered_digits = [c for c in binary_part if c != '0']
+                        if filtered_digits:
+                            print("Цифры числа, исключая нули:", " ".join(filtered_digits))
             work_buffer = ""
 
         buffer = file.read(buffer_len)
@@ -46,4 +57,3 @@ if valid_numbers:
     print("Среднее число прописью:", " ".join(digit_to_text(d) for d in str(avg_value)))
 else:
     print("Не найдено подходящих чисел.")
-
